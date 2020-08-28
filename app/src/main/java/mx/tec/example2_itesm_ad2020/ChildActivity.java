0f31094d9bd18f11ef210115e89fde5a4ed26dd8
EricBL3 <ericbuitron3@gmail.com>
2020-08-27 20:27:28 -0500

    Add back button and image

--- app/src/main/java/mx/tec/example2_itesm_ad2020/ChildActivity.java
+++ app/src/main/java/mx/tec/example2_itesm_ad2020/ChildActivity.java
@@ -2,13 +2,30 @@ package mx.tec.example2_itesm_ad2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChildActivity extends AppCompatActivity {
+
+    Button myButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+
+        myButton = findViewById(R.id.backBtn);
+        myButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(ChildActivity.this, MainActivity.class);
+                startActivity(myIntent);
+
+            }
+        });
     }
 }
